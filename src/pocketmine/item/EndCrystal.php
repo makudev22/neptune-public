94963ad77805a767838b8dbf9417877762ede7a5
makudev22 <mansurerus3@gmail.com>
2026-09-23 16:30:14 +0500

    fix: correct end crystal placement and creative use

--- src/pocketmine/item/EndCrystal.php
+++ src/pocketmine/item/EndCrystal.php
@@ -12,6 +12,7 @@ use pocketmine\entity\Entity;
 use pocketmine\entity\object\EnderCrystal;
 use pocketmine\level\Location;
 use pocketmine\math\AxisAlignedBB;
+use pocketmine\math\Facing;
 use pocketmine\math\Vector3;
 use pocketmine\Player;
 
@@ -26,16 +27,18 @@ class EndCrystal extends Item
 
 	public function onActivate(Player $player, Block $blockReplace, Block $blockClicked, int $face, Vector3 $clickVector) : bool
 	{
-		if ($blockClicked->getId() === self::OBSIDIAN || $blockClicked instanceof Bedrock) {
+		if ($face === Facing::UP && ($blockClicked->getId() === self::OBSIDIAN || $blockClicked instanceof Bedrock)) {
 			$level = $blockClicked->getLevel();
-			$entities = $level->getNearbyEntities(new AxisAlignedBB($blockClicked->getX(), $blockClicked->getY(), $blockClicked->getZ(), $blockClicked->getX() + 1, $blockClicked->getY() + 2, $blockClicked->getZ() + 1));
+			$entities = $level->getNearbyEntities(new AxisAlignedBB($blockClicked->getX(), $blockClicked->getY() + 1, $blockClicked->getZ(), $blockClicked->getX() + 1, $blockClicked->getY() + 3, $blockClicked->getZ() + 1));
 			if (count($entities) === 0 && $level->getBlock($blockClicked->up()) instanceof Air && $level->getBlock($blockClicked->up(2)) instanceof Air) {
-				$nbt = Entity::createBaseNBT(Location::fromObject($blockClicked->add(0.5, 1.5, 0.5)));
+				$nbt = Entity::createBaseNBT(Location::fromObject($blockClicked->add(0.5, 1, 0.5)));
 
 				$crystal = new EnderCrystal($level, $nbt);
 				$crystal->spawnToAll();
 
-				$this->pop();
+				if ($player->isSurvival()) {
+					$this->pop();
+				}
 
 				return true;
 			}
